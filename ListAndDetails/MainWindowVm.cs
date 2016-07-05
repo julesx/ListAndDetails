bc6a004ec3ffc143f4417f3888966a218cc22885
Julien Rosen <jlrosen416@gmail.com>
2016-07-05 09:47:55 -0400

    Add project files.

---
+++ ListAndDetails/MainWindowVm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListAndDetails
+{
+    public class MainWindowVm
+    {
+        public ObservableCollection<PersonVm> People { get; set; } = new ObservableCollection<PersonVm>();
+        public PersonVm SelectedPerson { get; set; }
+
+        public MainWindowVm()
+        {
+            PopulatePeople();
+        }
+
+        private void PopulatePeople()
+        {
+            People.Add(new PersonVm("Maverik", "Real OG"));
+            People.Add(new PersonVm("Johan", "Wizard"));
+            People.Add(new PersonVm("Julien", "Apprentice"));
+            People.Add(new PersonVm("C4u", "Pupil"));
+            People.Add(new PersonVm("Alex", "New Hire"));
+        }
+    }
+}
---
+++ ListAndDetails/PersonVm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PropertyChanged;
+
+namespace ListAndDetails
+{
+    [ImplementPropertyChanged]
+    public class PersonVm
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public double Salary { get; set; } = new Random().NextDouble() * 100;
+        public DateTime DateOfBirth { get; set; } = DateTime.UtcNow.AddDays(new Random().Next(90));
+
+        public PersonVm(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
